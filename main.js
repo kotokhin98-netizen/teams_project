9405408ecc5770633bed1d1cd004d15e9349b45b
ALASTOR <yaroslav.tolstukhin@mail.ru>
2026-01-19 12:05:26 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// js/main.js
+console.log('CyberZone сайт загружен!');
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
